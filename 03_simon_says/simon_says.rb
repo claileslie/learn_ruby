eb72d8582f5668bbbfb9742bf0f5dc7d23ee0f2e
CLAIRE LESLIE <c.leslie@live.ca>
2017-01-23 17:41:45 -0500

    add echo and shout methods to simon_says.rb

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,14 @@
+def echo(phrase)
+  "#{phrase}"
+end
+
+def shout(phrase)
+  "#{phrase.upcase}"
+end
+
+def repeat(phrase, num = 2)
+  "#{phrase * num} ".strip
+end
+
+def start_of_word(phrase)
+end
